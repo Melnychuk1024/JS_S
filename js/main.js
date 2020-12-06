60d9bf9064dff64f60a14fb1582df2a669276c9b
Dima <melnychuk.ss.1024@gmail.com>
2020-12-06 18:47:57 +0200

    lessons 061 slider_1

--- js/main.js
+++ js/main.js
@@ -222,3 +222,52 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 fetch('http://localhost:3000/menu')
     .then(data => data.json())
     .then(res => console.log(res));
+
+//SLIDER
+
+const slides = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current');
+
+let slideIndex = 1;
+
+if(slides < 10) {
+    total.textContent = `0${slides.length}`
+} else {
+    total.textContent = slides.length;
+}
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if(n < 1) {
+        slideIndex = slides.length;
+    }
+
+    slides.forEach(item => item.style.display = 'none');
+    slides[slideIndex - 1].style.display = 'block';
+
+    if(slides < 10) {
+        current.textContent = `0${slideIndex}`
+    } else {
+        current.textContent = slideIndex;
+    }
+} 
+
+function plussSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+prev.addEventListener('click', () => {
+    plussSlides(-1);
+});
+
+next.addEventListener('click', () => {
+    plussSlides(1);
+});
